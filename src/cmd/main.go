e999ecc01faf9127afc15f62da5c355919fb0e7a
Swaraj Pradhan <tts312@outlook.com>
2026-07-24 21:01:52 +0530

    [diyd] add server and app
    - add server and app

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,24 +2,16 @@ package main
 
 import (
 	"diyd/src/cmd/server"
-	"diyd/src/database"
-	"diyd/src/database/worker"
+	"diyd/src/config"
 	"flag"
 )
 
-type cmdArgs struct {
-	kvName   string
-	create bool
-	port     int
-	maxConns int
-}
-
-func parseArgs() cmdArgs {
-	args := cmdArgs{}
-	args.kvName = *flag.String("kvstore", "default_kvstore", "use -kvstore <name> to select the store")
-	args.create = *flag.Bool("create", false, "use -create to create a new kvstore")
-	args.port = *flag.Int("port", 8080, "use -port <no>")
-	args.maxConns = *flag.Int("maxconn", 1000, "use -maxconn <no>")
+func parseArgs() config.CmdArgs {
+	args := config.CmdArgs{}
+	args.KVName = *flag.String("kvstore", "default_kvstore", "use -kvstore <name> to select the store")
+	args.Create = *flag.Bool("create", false, "use -create to create a new kvstore")
+	args.Port = *flag.Int("port", 8080, "use -port <no>")
+	args.MaxCons = *flag.Int("maxconn", 1000, "use -maxconn <no>")
 	flag.Parse()
 
 	return args
@@ -27,19 +19,8 @@ func parseArgs() cmdArgs {
 
 func main() {
 	args := parseArgs()
-
-	dbparams := []database.Params{database.WithTable(args.kvName)}
-	if args.create {
-		dbparams = append(dbparams, database.WithCreateTable(args.kvName))
-	}
-	kvs, _ := database.NewKVStore(dbparams...)
-	kvw := worker.NewKVWorker(worker.Params{
-		WC:      10,
-		KVStore: kvs,
-	})
 	s := &server.Server{
-		Port:   args.port,
-		Worker: kvw,
+		Port: args.Port,
 	}
 	s.Start()
 }
